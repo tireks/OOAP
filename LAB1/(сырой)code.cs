3bdfd499cf25745363b90f2279ea823736877f95
Danila Afanasyev <145258448+danyachd@users.noreply.github.com>
2023-10-14 14:18:51 +0700

    Lab1and2Code.cs

--- LAB1/(сырой)code.cs
+++ LAB1/(сырой)code.cs
@@ -1,121 +1,155 @@
-//на обсуждение:
-//    1)    В ConfigCatalog - field:type  пропустил в коде
-//    2)    В VendorDevice.Execute/ControlSwitch.Execute/IPlugin.Execute сейчас плейсхолдер бам
-//    3)    зависимости изображены ф-иями UseXXXXX, мб изменить на что-то более близкое к предмет области
+    public class ControlSwitch
+    {
+        public string ControlSwitchName;
+        public int ControlSwitchCondition;
 
-public class ControlSwitch
-{
-    public string ControlSwitchName;
-    public int ControlSwitchCondition;
+        public void Execute(string Command)
+        {
+            Console.WriteLine("бам");
+        }
+    }
+    public class SensorReading
+    {
+        public string SensorName;
+        public int SensorValue;
 
-    public void Execute(string Command)
-    {
-        Console.WriteLine("бам");
+        public float GetValue()
+        {
+            return SensorValue;
+        }
     }
-}
-public class SensorReading
-{
-    public string SensorName;
-    public int SensorValue;
+    public class VendorDevice : IPlugin 
+    {
+        public string VendorDeviceName;
+        public string VendorDeviceManufacturer;
+        public string VendorDeviceEcosystem;
+        public int VendorDeviceCondition;
 
-    public float GetValue()
-    {
-        return SensorValue;
+        //реализация метода из IPlugin
+        public void Execute(string Command) //реализация метода из IPlugin
+        {
+            Console.WriteLine("бам");
+        }
     }
-}
-public class VendorDevice : IPlugin 
-{
-    public string VendorDeviceName;
-    public string VendorDeviceManufacturer;
-    public string VendorDeviceEcosystem;
-    public int VendorDeviceCondition;
+    public interface IPlugin //реализация: определение интерфейса и его реализация в классе VendorDevice
+    {
+        public void Execute(string Command)
+        {
+        }
+        public void UseIPlugin()
+        {
 
-    public void Execute(string Command)
-    {
-        Console.WriteLine("бам");
+        }
     }
-}
-public interface IPlugin //реализация: определение интерфейса и его реализация в классе VendorDevice
-{
-    public void Execute(string Command)
+    public interface IFunctional //реализация: определение интерфейса и его реализация в классах SubServer и ConfigCatalog
     {
-        Console.WriteLine("бам");
-    }
-    public void UseIPlugin()
-    {
+        public void FuncTurnOn(bool Value)
+        {
 
-    }
-}
-public interface IFunctional //реализация: определение интерфейса и его реализация в классах SubServer и ConfigCatalog
-{
-    public void FuncTurnOn(bool Value)
-    {
+        }
+        public void FuncTurnOff(bool Value)
+        {
 
-    }
-    public void FuncTurnOff(bool Value)
-    {
+        }
+        public void FuncChangeBrightness(int Value)
+        {
 
-    }
-    public void FuncChangeBrightness(int Value)
-    {
+        }
+        public void FuncOpen(bool Value)
+        {
 
-    }
-    public void FuncOpen(bool Value)
-    {
+        }
+        public void FuncClose(bool Value)
+        {
 
-    }
-    public void FuncClose(bool Value)
-    {
+        }
+        public void UseIFunctional()
+        {
 
+        }
     }
-    public void UseIFunctional()
+    public abstract class SubServer : IFunctional
     {
+        public string Name;
 
-    }
-}
-public abstract class SubServer : IFunctional
-{
-    public string Name;
+        public SensorReading []sensorreading; //агрегация
+        public ControlSwitch []controlswitch;
 
-    public SensorReading sensorreading;
-    public ControlSwitch controlswitch;
+        public void ObserveConfigCatalog(ConfigCatalog c) // зависимость: изменения в ConfigCatalog могут повлечь изменения в SubServer
+        {
+            c.GetState();
+        }
 
-    public SubServer(SensorReading sensorRe, ControlSwitch controlSw)
-    {
-        sensorreading = sensorRe;   // агрегация: класс SubServer содержит объекты классов SensorReading и ControlSwitch,
-        controlswitch = controlSw;  // при уничтожении объекта SubServer объекты SensorReading и ControlSwitch не будет уничтожены.
+        public void FuncTurnOn(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncTurnOff(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncChangeBrightness(int Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncOpen(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncClose(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void UseIFunctional()
+        {
+            Console.WriteLine("бам");
+        }
     }
-    public void UseConfigCatalog(ConfigCatalog c) // зависимость: изменения в ConfigCatalog могут повлечь изменения в SubServer
+    public class ConfigCatalog : IFunctional
     {
-        c.UseConfigCatalog();
-    }
-}
-public class ConfigCatalog : IFunctional
-{
-    public void UseConfigCatalog()
-    {
+        public string []entities;
+        public void GetState()
+        {
+            
+        }
 
+        // реализации методов из IFunctional
+        public void FuncTurnOn(bool Value) 
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncTurnOff(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncChangeBrightness(int Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncOpen(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void FuncClose(bool Value)
+        {
+            Console.WriteLine("бам");
+        }
+        public void UseIFunctional()
+        {
+            Console.WriteLine("бам");
+        }
     }
-}
-public class CentralServer
-{
-    public ConfigCatalog configcatalog;
-    public SubServer subserver;
-
-    public CentralServer(SubServer subS)
+    public class CentralServer
     {
-        configcatalog = new ConfigCatalog(); // композиция: класс CentralServer содержит объект класса ConfigCatalog,
-                                             // при уничтожении объекта CentralServer уничтожится и объект ConfigCatalog.
-
-        subserver = subS; // агрегация: класс CentralServer содержит объект класса SubServer,
-                          // при уничтожении объекта CentralServer объект SubServer не будет уничтожен.
+        public ConfigCatalog configcatalog = new ConfigCatalog(); //композиция
+        public SubServer []boards; //агрегация
+   
+        public void UseIPlugin(IPlugin p) // зависимость: изменения в IPlugin могут повлечь изменения в CentralServer
+        {
+            p.UseIPlugin();
+        }
+        public void UseIFunctional(IFunctional f) //зависимость, аналогично
+        {
+            f.UseIFunctional();
+        }
     }
-    public void UseIPlugin(IPlugin p) // зависимость: изменения в IPlugin могут повлечь изменения в CentralServer
-    {
-        p.UseIPlugin();
-    }
-    public void UseIFunctional(IFunctional f) //зависимость, аналогично
-    {
-        f.UseIFunctional();
-    }
-}
