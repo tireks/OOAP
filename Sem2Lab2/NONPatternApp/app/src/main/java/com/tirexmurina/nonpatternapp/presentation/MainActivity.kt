a2a5b830e786536a302a84413f73b1bc0bb1ec2a
tireks <33616425+tireks@users.noreply.github.com>
2024-03-21 15:37:19 +0700

    init

---
+++ Sem2Lab2/NONPatternApp/app/src/main/java/com/tirexmurina/nonpatternapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tirexmurina.nonpatternapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tirexmurina.nonpatternapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
